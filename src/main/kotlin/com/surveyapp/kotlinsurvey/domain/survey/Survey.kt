ec538297bb71148f1fce68d7c52999eda5cbb72d
HIredrain <redrain@yu.ac.kr>
2024-04-19 02:36:01 +0900

    commit message

--- src/main/kotlin/com/surveyapp/kotlinsurvey/domain/survey/Survey.kt
+++ src/main/kotlin/com/surveyapp/kotlinsurvey/domain/survey/Survey.kt
@@ -8,25 +8,24 @@ import java.util.*
 class Survey(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increasement
-    val survey_id: Int? = null,
+    val survey_id: Int? = null, // 설문 조사 게시 글 ID
 
     @ManyToOne
     @JoinColumn(name="member_id", referencedColumnName = "id")
-    val user: User,
+    val user: User, // 설문 조사 게시 글 작성자
 
     @Column
-    val title : String,
+    val title : String, // 설문 조사 게시 글 제목
 
     @Column
-    val discription : String,
+    val discription : String, // 설문 조사 게시 글 내용
 
     @Column
-    val start_date : Date,
+    val start_date : Date, // 설문 조사 시작일
 
     @Column
-    val end_date : Date,
+    val end_date : Date, // 설문 조사 종료일
 
 ) {
-
 
 }
--- src/main/kotlin/com/surveyapp/kotlinsurvey/domain/user/User.kt
+++ src/main/kotlin/com/surveyapp/kotlinsurvey/domain/user/User.kt
@@ -7,50 +7,51 @@ import jakarta.persistence.*
 @Entity
 @Table(name = "tb_user")
 class User(
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increasement
-    val member_id: Int? = null,
+    val member_id: Int? = null, // 회원 ID - 정수
 
     @Column
-    val user_id : String,
+    val user_id : String, // 사용자 ID
 
     @Column
-    val user_pw : String,
+    val user_pw : String, // 사용자 비밀번호
 
     @Column
-    val user_nm: String,
+    val user_nm: String, // 사용자 이름
 
     @Column
-    val phone : String,
-
-    @Column
-    @Enumerated(EnumType.STRING)
-    val type: UserType,
+    val phone_number : String, // 사용자 전화번호
 
     @Column
     @Enumerated(EnumType.STRING)
-    val gender : GenderType,
+    val type: UserType, // 사용자 유형 : 회원, 관리자
 
+    @Column
+    @Enumerated(EnumType.STRING)
+    val gender : GenderType, // 사용자 성별
+
+    // class User : class UserInquiry 를 1:n mapping
     @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
     val userInquiries : MutableList<UserInquiry> = mutableListOf(),
 
+    // class User : class Survey 를 1:n mapping
     @OneToMany(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
     val surveys : MutableList<Survey> = mutableListOf(),
 
     ) {
-    init {
+    init { // 초기화
         if (user_id.isBlank()) {
-            throw IllegalArgumentException("아이디는 비어 있을 수 없습니다")
+            throw IllegalArgumentException("ID 를 전달받지 못했습니다.")
         }
         if (user_pw.isBlank()) {
-            throw IllegalArgumentException("비밀번호는 비어 있을 수 없습니다")
+            throw IllegalArgumentException("비밀번호를 전달받지 못했습니다.")
         }
         if (user_nm.isBlank()) {
-            throw IllegalArgumentException("이름은 비어 있을 수 없습니다")
+            throw IllegalArgumentException("이름을 전달받지 못했습니다.")
         }
-        if (phone.isBlank()) {
-            throw IllegalArgumentException("전화번호는 비어 있을 수 없습니다")
+        if (phone_number.isBlank()) {
+            throw IllegalArgumentException("전화번호를 전달받지 못했습니다.")
         }
     }
 
