4af72f18e1df1b952eb0bd274769511dd42bc5d2
HIredrain <redrain@yu.ac.kr>
2024-05-01 06:38:46 +0900

    commit massage

--- src/main/kotlin/com/surveyapp/kotlinsurvey/controller/SurveyPostController.kt
+++ src/main/kotlin/com/surveyapp/kotlinsurvey/controller/SurveyPostController.kt
@@ -3,21 +3,16 @@ package com.surveyapp.kotlinsurvey.controller
 import com.surveyapp.kotlinsurvey.domain.survey.*
 import com.surveyapp.kotlinsurvey.service.SurveyService
 
-import jakarta.servlet.http.HttpSession
-import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
-import java.time.LocalDate
 
 
 //@RequestMapping("/post") // endpoint
 @Controller
-class SurveyPostContorller(
+class SurveyPostController(
         @Autowired private val surveyService: SurveyService,
 ) {
 
