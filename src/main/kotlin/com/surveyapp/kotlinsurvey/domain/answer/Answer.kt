db21ed05ba6be73fc4956a9b42a6afbb07240a01
HIredrain <redrain@yu.ac.kr>
2024-05-05 19:52:19 +0900

    띄어 쓰기 수정

--- src/main/kotlin/com/surveyapp/kotlinsurvey/domain/answer/Answer.kt
+++ src/main/kotlin/com/surveyapp/kotlinsurvey/domain/answer/Answer.kt
@@ -16,11 +16,11 @@ abstract class Answer(
 
     @ManyToOne
     @JoinColumn(name="user_id", foreignKey = ForeignKey(name = "fk_user_id"))
-    open val user : User, // 멤버 하나 당 여러개의 답변이 있을 수 있다.
+    open val user : User, // 멤버 하나당 여러개의 답변이 있을 수 있다.
 
     @ManyToOne
     @JoinColumn(name = "question_id", foreignKey = ForeignKey(name = "fk_question_id"))
-    open val question: Question, // 질문 하나에 대한 여러개의 답변이 있을 수 있다.
+    open val question: Question, // 질문 하나당 여러 개의 답변이 있을 수 있다.
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "participation_id", nullable = false)
