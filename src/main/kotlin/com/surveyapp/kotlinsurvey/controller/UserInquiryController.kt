762382cf0a55a8eb1dfccf58b71b991277e25064
skaigroove <skaigroove@gmail.com>
2024-05-25 16:43:37 +0900

    - 문의글 디자인 수정

--- src/main/kotlin/com/surveyapp/kotlinsurvey/controller/UserInquiryController.kt
+++ src/main/kotlin/com/surveyapp/kotlinsurvey/controller/UserInquiryController.kt
@@ -26,18 +26,28 @@ class UserInquiryController(
     @Autowired private val userService: UserService,
 ) {
     @GetMapping("")
-    fun listInquiry(model: Model): String { // 문의 게시 글 목록
-        val inquiryList = userInquiryService.getInquiryList()
+    fun listInquiry(model: Model,session: HttpSession): String { // 문의 게시 글 목록
 
+        val inquiryList = userInquiryService.getInquiryList()
         model.addAttribute("inquiryList", inquiryList)
+
+        // 세션에서 사용자 이름 가져오기
+        val username = session.getAttribute("username")
+        print("username: $username")
+        model.addAttribute("username", username);
 
         return "inquiry" // 경로 반환 : inquiry.html
     }
 
     @GetMapping("/post")
-    fun createInquiryForm(model: Model): String {
+    fun createInquiryForm(model: Model, session: HttpSession): String {
         // model 에 UserInquiryForm 추가
         model.addAttribute("userInquiryForm", UserInquiryForm())
+
+        // 세션에서 사용자 이름 가져오기
+        val username = session.getAttribute("username")
+        print("username: $username")
+        model.addAttribute("username", username);
 
         return "createInquiryForm" // 경로 반환 : createInquiryForm.html
     }
@@ -88,11 +98,16 @@ class UserInquiryController(
     }
 
     @GetMapping("/{inquiryId}")
-    fun detailInquiry(@PathVariable inquiryId: Long, model: Model): String{ // 문의 글 상세 보기 관련 처리
+    fun detailInquiry(@PathVariable inquiryId: Long, model: Model,session: HttpSession): String{ // 문의 글 상세 보기 관련 처리
 
         val inquiryPost: UserInquiry = userInquiryService.getInquiryById(inquiryId)
 
         println("Status: ${inquiryPost.status}")
+
+        // 세션에서 사용자 이름 가져오기
+        val username = session.getAttribute("username")
+        print("username: $username")
+        model.addAttribute("username", username);
 
         // 속성 추가
         model.addAttribute("inquiryPost", inquiryPost)
