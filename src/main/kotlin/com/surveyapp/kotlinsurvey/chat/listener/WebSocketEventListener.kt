6e49d8b766cb91b452c6ebc5397bb4c053371998
skaigroove <skaigroove@gmail.com>
2024-05-25 23:06:06 +0900

    - 유저단 접속중인 사용자 구현 완료

--- src/main/kotlin/com/surveyapp/kotlinsurvey/chat/listener/WebSocketEventListener.kt
+++ src/main/kotlin/com/surveyapp/kotlinsurvey/chat/listener/WebSocketEventListener.kt
@@ -2,34 +2,47 @@ package com.surveyapp.kotlinsurvey.chat.listener
 
 import com.surveyapp.kotlinsurvey.chat.domain.ChatMessage
 import com.surveyapp.kotlinsurvey.chat.domain.MessageType
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
-import org.springframework.messaging.simp.SimpMessageSendingOperations
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.messaging.SessionConnectedEvent
 import org.springframework.web.socket.messaging.SessionDisconnectEvent
 
 @Component
 class WebSocketEventListener(
-    private val messagingTemplate: SimpMessageSendingOperations?
+    private val messagingTemplate: SimpMessagingTemplate
 ) {
-    private val logger: Logger = LoggerFactory.getLogger(WebSocketEventListener::class.java)
+
+    private val logger = LoggerFactory.getLogger(WebSocketEventListener::class.java)
+    private val onlineUsers = mutableSetOf<String>()
 
     @EventListener
-    fun handleWebSocketConnectListener(event: SessionConnectedEvent?) {
+    fun handleWebSocketConnectListener(event: SessionConnectedEvent) {
         logger.info("Received a new web socket connection")
     }
 
     @EventListener
     fun handleWebSocketDisconnectListener(event: SessionDisconnectEvent) {
-        val headerAccessor = StompHeaderAccessor.wrap(event.message)
-        val username = headerAccessor.sessionAttributes!!["username"] as String?
+        val headerAccessor = event.message.headers[SimpMessageHeaderAccessor::class.java.name] as SimpMessageHeaderAccessor
+        val username = headerAccessor.sessionAttributes?.get("username") as String?
+
         if (username != null) {
-            logger.info("User Disconnected : $username")
-            val chatMessage = ChatMessage(MessageType.LEAVE, "", username)
-            messagingTemplate!!.convertAndSend("/topic/public", chatMessage)
+            logger.info("User Disconnected: $username")
+            onlineUsers.remove(username)
+            val chatMessage = ChatMessage(sender = username, content = "", type = MessageType.LEAVE)
+            messagingTemplate.convertAndSend("/topic/public", chatMessage)
+            messagingTemplate.convertAndSend("/topic/onlineUsers", onlineUsers)
         }
+    }
+
+    fun addUser(username: String) {
+        onlineUsers.add(username)
+        messagingTemplate.convertAndSend("/topic/onlineUsers", onlineUsers)
+    }
+
+    fun getOnlineUsers(): Set<String> {
+        return onlineUsers
     }
 }
--- src/main/kotlin/com/surveyapp/kotlinsurvey/controller/ChatController.kt
+++ src/main/kotlin/com/surveyapp/kotlinsurvey/controller/ChatController.kt
@@ -1,25 +1,34 @@
 package com.surveyapp.kotlinsurvey.controller
 
 import com.surveyapp.kotlinsurvey.chat.domain.ChatMessage
+import com.surveyapp.kotlinsurvey.chat.domain.MessageType
+import com.surveyapp.kotlinsurvey.chat.listener.WebSocketEventListener
 import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.messaging.handler.annotation.SendTo
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.stereotype.Controller
 
+@Controller
+class ChatController(
+    private val messagingTemplate: SimpMessagingTemplate,
+    private val webSocketEventListener: WebSocketEventListener
+) {
 
-@RestController
-class ChatController {
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
-    fun sendMessage(@Payload chatMessage: ChatMessage?): ChatMessage? {
-        return chatMessage
+    fun sendMessage(message: ChatMessage): ChatMessage {
+        return message
     }
 
     @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    fun addUser(@Payload chatMessage: ChatMessage, headerAccessor: SimpMessageHeaderAccessor): ChatMessage? {
-        headerAccessor.sessionAttributes!!["username"] = chatMessage.sender
-        return chatMessage
+    fun addUser(message: ChatMessage) {
+        webSocketEventListener.addUser(message.sender)
+        val chatMessage = ChatMessage(sender = message.sender, content = "", type = MessageType.JOIN)
+        messagingTemplate.convertAndSend("/topic/public", chatMessage)
+    }
+
+    @MessageMapping("/chat.getOnlineUsers")
+    fun getOnlineUsers() {
+        messagingTemplate.convertAndSend("/topic/onlineUsers", webSocketEventListener.getOnlineUsers())
     }
 }
