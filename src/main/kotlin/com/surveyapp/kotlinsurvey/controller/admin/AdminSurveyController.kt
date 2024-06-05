8a7996fa64dc155098140c505b8268791bfdc988
HIredrain <redrain@yu.ac.kr>
2024-06-06 03:29:37 +0900

    관리자 (admin) : 설문 관리 - 안 쓰는 함수 정리

--- src/main/kotlin/com/surveyapp/kotlinsurvey/controller/admin/AdminSurveyController.kt
+++ src/main/kotlin/com/surveyapp/kotlinsurvey/controller/admin/AdminSurveyController.kt
@@ -49,22 +49,6 @@ class AdminSurveyController(
         return "admin-auth/admin-survey/admin-survey-detail" // 경로 반환
     }
 
-    /*
-    @PostMapping("/survey/delete/{surveyId}")
-    fun adminSurveyDelete(@PathVariable surveyId: Long, session: HttpSession): String { // 관리자 (admin) - 설문 삭제 관련 처리
-        // login 여부 확인
-        if (userService.checkLogin(session) == null) // 로그인 안 되었음 => null 반환됨
-            return "redirect:/"
-
-        //val survey = surveyService.getSurveyById(surveyId) // surveyId 에 해당하는 설문 get
-
-        surveyService.deleteSurvey(surveyId) // 해당 설문 삭제
-
-        return "redirect:/admin/survey"
-    }
-
-     */
-
     @PostMapping("/survey/delete/{surveyId}")
     @ResponseBody
     fun adminSurveyDelete(@PathVariable surveyId: Long, session: HttpSession): ResponseEntity<String> { // 관리자 (admin) - 설문 삭제 관련 처리
